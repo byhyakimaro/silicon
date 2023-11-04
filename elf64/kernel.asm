d4054691917dcd94173a03272befb3a58ff339f5
byhyakimaro <moderrevolutio090217@gmail.com>
2023-11-04 21:01:06 +0000

    save

--- elf64/kernel.asm
+++ elf64/kernel.asm
@@ -7,10 +7,10 @@ include 'libs/fs_lib.inc'
 segment readable executable
 entry main
 main:
-  write STDOUT, exit_c, exit_c_sz
-  
   .error_open:
+    write STDOUT, err_open, err_open_sz
     exit -1
+  
   open filename, O_RDONLY, ch_file
   mov rdi, rax
   cmp rdi, 0
@@ -21,12 +21,18 @@ main:
   write STDOUT, buffer, rax
   close rdi
 
+  write STDOUT, exit_c, exit_c_sz
   exit 0
 
 segment readable writeable
   exit_c db 'compiler ended process',0xA
   exit_c_sz = $-exit_c
+
+  err_open db 'error open file',0xA
+  err_open_sz = $-exit_c
+  
+  buffer rb 1024
+  buffer_sz = $-buffer
   
   filename db 'script.sl', 0
-  buffer db buffer_size  
-  buffer_sz = 100
+  ch_file dd 0
