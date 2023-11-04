37ce38d8ff914fbc4c58115ab6743cf26fd7b858
byhyakimaro <moderrevolutio090217@gmail.com>
2023-11-04 20:28:59 +0000

    sas

--- elf64/kernel.asm
+++ elf64/kernel.asm
@@ -4,19 +4,23 @@ include 'libs/references.inc'
 include 'libs/std_lib.inc'
 include 'libs/fs_lib.inc'
 
-buffer_size equ 1024
-buffer rb buffer_size
-
 segment readable executable
 entry main
 main:
-  open filename, O_RDONLY, ch_file
   write STDOUT, exit_c, exit_c_sz
+  
+  open filename, O_RDONLY, ch_file
+  mov rdi, rax
+  read rdi, buffer, 100
+  write STDOUT, buffer, rax
+  close rdi
+
   exit 0
 
 segment readable writeable
   exit_c db 'compiler ended process',0xA
   exit_c_sz = $-exit_c
   
-  filename db 'test/script.sl', 0x10
-  ch_file dd 0664
+  filename db 'test/script.sl', 0
+  buffer rb 100 
+  ch_file dd 0
