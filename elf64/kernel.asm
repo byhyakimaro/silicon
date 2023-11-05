23c4b5d862c72cf0c4cfe0549104bcaa2f959a63
byhyakimaro <moderrevolutio090217@gmail.com>
2023-11-05 04:21:03 +0000

    save

--- elf64/kernel.asm
+++ elf64/kernel.asm
@@ -12,32 +12,34 @@ error_open:
 segment readable executable
 entry main
 main:
+
+  stat filename, stat_data
+  mov rdi, stat_data
+  add rdi, 48
+  mov r10, [rdi]
+
+  malloc ptr_to_buffer, rsi
+  
   open filename, O_RDONLY, ch_file
   mov rdi, rax
   cmp rdi, 0
   jl error_open
-
-  stat filename, ptr_to_memory
-  int3
   
-  read rdi, buffer, buffer_size
+  read rdi, ptr_to_buffer, r10
   cmp rax, 0
   jle error_open
 
-  malloc ptr_to_memory, 100
-  free ptr_to_memory
-
-  write STDOUT, buffer, rcx
+  write STDOUT, ptr_to_buffer, rcx
   close rdi
+
+  free ptr_to_buffer
   exit 0
 
 segment readable writeable
   filename db 'script.sl', 0
+  ptr_to_buffer dq 0
+  stat_data dq 0
   ch_file dd 0
 
   err_open db 'error open file', 0xA
   err_open_sz = $-err_open
-
-  buffer_size = 1024
-  buffer rb buffer_size
-  ptr_to_memory dq 0
